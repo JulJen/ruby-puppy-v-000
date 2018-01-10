1b117ce1414634566071c1693c40f4818d3e5b22
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-10 04:09:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+
   @@all =[]
+
+  attr_accessor :name
+
+ def initialize(name)
+   @name = name
+   @@all << self
+ end
+
+ def self.all
+   @@all.each do |x|
+     puts x.name
+   end
+ end
 end
